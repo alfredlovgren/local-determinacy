29d195229642e955126fb6af6dd418146cade302
Alfred <alfredlovgr1@gmail.com>
2024-05-02 14:37:02 +0200

    Speed

--- src/local-determancy/functions.py
+++ src/local-determancy/functions.py
@@ -47,41 +47,4 @@ def windingNumberCounterClockwise(F):
     return sum((1 if (F[i].imag > 0) and (F[i].real*F[i-1].real < 0) and (F[i].real < F[i-1].real)  else 0) for i in range(len(F)))
 
 def onatskiWindingNumber(F):
-    return windingNumberClockwise(F) + windingNumberCounterClockwise(F)
-
-def checkSolutions(F):
-    #### Interpretation of winding number:
-    #### Winding number CW (Multiple Solution)
-    #### Winding number CCW (No Solution)
-    winding_out = "Winding number: " + str(F)
-    if F == 0:
-        return(winding_out + "\nThe economy is DETERMINATE")
-    
-    elif F > 0:
-        return(winding_out + "\nThe economy is INDETERMINATE (NO SOLUTION)")    
-    
-    elif F < 0:
-        return(winding_out + "\nThe economy is INDETERMINATE (MULTIPLE SOLUTION)") 
-
-def plot(F):
-    plt.plot(F.real, F.imag, color='blue',linewidth=3)
-
-    plt.xlabel('Real - axis', fontsize=18)
-
-    plt.ylabel('Imaginary - axis', fontsize=18)
-
-    plt.title('Onatski graph')
-
-    angle = np.deg2rad(45)
-
-    cross_length = max(max(F.real)-min(F.real), max(F.imag)-min(F.imag)) * 0.065
-
-    plt.plot([-cross_length * np.cos(angle), cross_length * np.cos(angle)],
-            [-cross_length * np.sin(angle), cross_length * np.sin(angle)],
-            color='red', linestyle='-', linewidth=3)  # Diagonal line /
-
-    plt.plot([-cross_length * np.cos(angle), cross_length * np.cos(angle)],
-            [cross_length * np.sin(angle), -cross_length * np.sin(angle)],
-            color='red', linestyle='-', linewidth=3)  # Diagonal line \
-
-    plt.show()
+    return windingNumberClockwise(F) + windingNumberCounterClockwi
